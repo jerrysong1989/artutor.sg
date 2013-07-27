32f4f6842d39e17ef1479b853f3e8772ce48c152
Tan Chen <admin8978boy@gmail.com>
2013-07-28 01:49:42 +0800

    Delete main.css as not using original theme

--- protected/views/site/pages/about.php
+++ protected/views/site/pages/about.php
@@ -1,12 +1,67 @@
-<?php
-/* @var $this SiteController */
 
-$this->pageTitle=Yii::app()->name . ' - About';
-$this->breadcrumbs=array(
-	'About',
-);
-?>
-<h1>About</h1>
 
-<p>This is a "static" page. You may change the content of this page
-by updating the file <code><?php echo __FILE__; ?></code>.</p>
+<div id="content-top">
+	<div class="divider-orange"></div>
+</div>
+
+<div id="content-mid" class="row">
+	<div class="span2">
+
+
+		<div id="content-leftmenu">
+			<ul class="nav nav-list">
+			  <li class="nav-header title">Catalogue</li>
+				  <li class="nav-header title">Service</li>
+				  <li class="nav-header title">About</li>
+					  <ul>
+					  <li class=""><a href="about.php">About ISHU</a></li>
+  					  <li class=""><a href="#">About artutor.sg</a></li>
+  					  <li class=""><a href="#">Partnership</a></li>
+					  </ul>
+				  <li class="nav-header title">About</li>
+			  <li class="nav-header title">Sitemap</li>
+			  <li class="nav-header title">FAQ</li>
+			</ul>
+		</div>
+  	</div>
+
+		<div class="span9">
+
+		<div id="content-rightheading" class="title page-heading">			
+			<img src="../resources/banner1.png" style="width:100%; margin-bottom:30px;">
+			<div class="clear"></div>
+			nI MEI A 
+		</div>
+
+		<div id="content-rightcontent">
+
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam 
+				pellentesque ipsum a suscipit dapibus. Maecenas placerat turpis iaculis 
+				tortor malesuada, quis aliquet metus consectetur. Sed vel quam neque. 
+				Vivamus tincidunt augue ante. Duis blandit laoreet mattis. Fusce leo nibh,
+				venenatis at dictum in, elementum in arcu. Sed volutpat nec dui vitae 
+				luctus. Sed tempus imperdiet massa, et consectetur ante pharetra a. 
+				Phasellus nec egestas ligula. Quisque aliquet, ligula a tincidunt pretium, 
+				nisl odio dapibus velit, luctus tincidunt quam velit quis sem. Maecenas 
+				congue ac risus eget tincidunt.
+			</p>
+			<p>
+				Quisque euismod venenatis justo, et posuere sem vulputate id. 
+				Mauris commodo tellus ac pulvinar interdum. Vestibulum convallis 
+				nibh felis, accumsan sagittis ante vehicula vel. Morbi congue nec ante 
+				ac laoreet. Suspendisse potenti. Fusce a eros nisl. Donec vitae venenatis
+				diam, at suscipit lorem. Suspendisse vitae sodales lectus. Ut hendrerit 
+				nisi est. Nullam vehicula semper velit. Nullam sapien ligula, laoreet eu 
+				elit in, porttitor rutrum elit. Vivamus et ante cursus turpis luctus posuere 
+				vel et metus. Maecenas at lacus risus. Aliquam dapibus nisl tortor, sed 
+				viverra neque pulvinar eu. Vestibulum in est varius, pulvinar diam a, 
+				euismod libero.
+			</p>
+		</div>
+	</div>
+
+
+</div>
+
+
